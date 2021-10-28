2acdc211554c899c801844b6e9913df1034a9d79
Naoyuki Ishida <39.ishida@gmail.com>
2021-10-27 18:08:18 -0700

    Refactoring user controller
    Delete set_user method.
    Because it makes complicated about instance variable.
    I don't want to use before_action for creating instance variables.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i(show)
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/1/edit
@@ -33,11 +33,6 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name)
