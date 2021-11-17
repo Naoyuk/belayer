c3ebdf27c0bc49f20f21fa837ee4994072640ef4
Naoyuki Ishida <39.ishida@gmail.com>
2021-11-17 13:37:50 -0800

    Update post request spec
    How defferent from an authenticated user and a guest user.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -24,114 +24,217 @@ RSpec.describe "Posts", type: :request do
 
   let(:user) { FactoryBot.create(:user) }
 
-  before do
-    sign_in user
-    @post = FactoryBot.create(:post, user: user)
-  end
-
-
-
   describe "GET /index" do
-    it "renders a successful response" do
-      get posts_url
-      expect(response).to be_successful
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "renders a successful response" do
+        get posts_url
+        expect(response).to be_successful
+      end
+
+      it "returns a 200 response" do
+        get posts_url
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    context 'as a guest' do
+      it "returns a 302 response" do
+        get posts_url
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirect to the sign-in page" do
+        get posts_url
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
   describe "GET /show" do
-    it "renders a successful response" do
-      get post_url(@post)
-      expect(response).to be_successful
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "renders a successful response" do
+        get post_url(@post)
+        expect(response).to be_successful
+      end
+    end
+
+    context 'as a guest' do
+      before do
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "returns a 302 response" do
+        get post_url(@post)
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirect to the sign-in page" do
+        get post_url(@post)
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
   describe "GET /new" do
-    it "renders a successful response" do
-      get new_post_url
-      expect(response).to be_successful
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "renders a successful response" do
+        get new_post_url
+        expect(response).to be_successful
+      end
+    end
+
+    context 'as a guest' do
+      it "returns a 302 response" do
+        get new_post_url
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirect to the sign-in page" do
+        get new_post_url
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
   describe "GET /edit" do
-    it "render a successful response" do
-      get edit_post_url(@post)
-      expect(response).to be_successful
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "render a successful response" do
+        get edit_post_url(@post)
+        expect(response).to be_successful
+      end
+    end
+
+    context 'as a guest' do
+      before do
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      it "returns a 302 response" do
+        get edit_post_url(@post)
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirect to the sign-in page" do
+        get edit_post_url(@post)
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 
   describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Post" do
+    context 'as an authenticated user' do
+      before do
         sign_in user
-        expect {
+        @post = FactoryBot.create(:post, user: user)
+      end
+
+      context "with valid parameters" do
+        it "creates a new Post" do
+          sign_in user
+          expect {
+            post posts_url, params: { post: valid_attributes, user: user }
+          }.to change(Post, :count).by(1)
+        end
+
+        it "redirects to the created post" do
+          sign_in user
           post posts_url, params: { post: valid_attributes, user: user }
-        }.to change(Post, :count).by(1)
+          expect(response).to redirect_to(post_url(Post.last))
+        end
       end
 
-      it "redirects to the created post" do
-        sign_in user
-        post posts_url, params: { post: valid_attributes, user: user }
-        expect(response).to redirect_to(post_url(Post.last))
-      end
-    end
+      context "with invalid parameters" do
+        it "does not create a new Post" do
+          expect {
+            post posts_url, params: { post: invalid_attributes }
+          }.to change(Post, :count).by(0)
+        end
 
-    context "with invalid parameters" do
-      it "does not create a new Post" do
-        expect {
+        it "renders a successful response (i.e. to display the 'new' template)" do
           post posts_url, params: { post: invalid_attributes }
-        }.to change(Post, :count).by(0)
-      end
-
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post posts_url, params: { post: invalid_attributes }
-        expect(response).to be_successful
+          expect(response).to be_successful
+        end
       end
     end
   end
 
   describe "PATCH /update" do
-    context "with valid parameters" do
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
 
-      let(:new_attributes) {
-        {
-          date: Date.tomorrow.strftime("%Y-%m-%d"),
-          start_time: Date.tomorrow.strftime("%H:%M"),
-          end_time: Date.tomorrow.strftime("%H:%M"),
-          kind_of_climbing: :multi_pitches
+      context "with valid parameters" do
+
+        let(:new_attributes) {
+          {
+            date: Date.tomorrow.strftime("%Y-%m-%d"),
+            start_time: Date.tomorrow.strftime("%H:%M"),
+            end_time: Date.tomorrow.strftime("%H:%M"),
+            kind_of_climbing: :multi_pitches
+          }
         }
-      }
 
-      it "updates the requested post" do
-        patch post_url(@post), params: { post: new_attributes }
-        @post.reload
-        expect(response).to redirect_to(post_url(@post))
+        it "updates the requested post" do
+          patch post_url(@post), params: { post: new_attributes }
+          @post.reload
+          expect(response).to redirect_to(post_url(@post))
+        end
+
+        it "redirects to the post" do
+          patch post_url(@post), params: { post: new_attributes }
+          @post.reload
+          expect(response).to redirect_to(post_url(@post))
+        end
       end
 
-      it "redirects to the post" do
-        patch post_url(@post), params: { post: new_attributes }
-        @post.reload
-        expect(response).to redirect_to(post_url(@post))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        patch post_url(@post), params: { post: invalid_attributes }
-        expect(response).to be_successful
+      context "with invalid parameters" do
+        it "renders a successful response (i.e. to display the 'edit' template)" do
+          patch post_url(@post), params: { post: invalid_attributes }
+          expect(response).to be_successful
+        end
       end
     end
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested post" do
-      expect {
-        delete post_url(@post)
-      }.to change(Post, :count).by(-1)
-    end
+    context 'as an authenticated user' do
+      before do
+        sign_in user
+        @post = FactoryBot.create(:post, user: user)
+      end
 
-    it "redirects to the posts list" do
-      delete post_url(@post)
-      expect(response).to redirect_to(posts_url)
+      it "destroys the requested post" do
+        expect {
+          delete post_url(@post)
+        }.to change(Post, :count).by(-1)
+      end
+
+      it "redirects to the posts list" do
+        delete post_url(@post)
+        expect(response).to redirect_to(posts_url)
+      end
     end
   end
 end
