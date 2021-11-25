a9b6368679c7aabbaf95ea1080160012e85ea18f
Naoyuki Ishida <39.ishida@gmail.com>
2021-11-24 23:45:57 -0800

    Migrate a mailer called new message notification

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,13 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.new_message.subject
+  #
+  def new_message
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
